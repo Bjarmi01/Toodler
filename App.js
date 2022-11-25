04ca739f72f5cf45244ceb5b9beb10a3cb3a27b1
saevar bjornsson <saevarl3@gmail.com>
2022-11-25 12:40:58 +0000

    bla

--- App.js
+++ App.js
@@ -1,24 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import { Main } from './src/views/main';
-<<<<<<< HEAD
-import { Task } from './src/components/task'
-
-export default function App() {
-  return (
-    <View >
-      <Task />
-      <StatusBar style="auto" />
-    </View>
-  );
-=======
 import AppContainer from './src/routes';
 
 export default function App() {
   return <AppContainer />;
     
   
->>>>>>> master
 }
 
 
